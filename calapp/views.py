5d532cc157c86839bd31f80c3b70e165ee23d9c9
Nikola B <nkl.bura@gmail.com>
2019-09-28 17:05:11 -0400

    Added view, at localhost:8000/cal/ which shows all the stuff in the database

--- calapp/views.py
+++ calapp/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Event
+from .models import Task
+from .models import Subtask
+from .models import Category
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the CALENDAR APP.")
+    all_events = Event.objects.order_by("event_name")
+    tasks = Task.objects.order_by("?")
+    subtasks = Subtask.objects.order_by("?")
+    categories = Category.objects.order_by("?")
+    template = loader.get_template("calapp/index.html")
+    context = {
+        "all_events": all_events,
+        "tasks": tasks,
+        "subtasks": subtasks,
+        "categories": categories,
+    }
+    return HttpResponse(template.render(context, request))
